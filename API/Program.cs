87ebdac3202428e55e7e79da8fe10eef6535fe70
Tara <t.nikpasand@gmail.com>
2024-11-15 17:37:27 +0000

    commit seventh

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,5 @@
 using API.Extensions;
-
+using API.Middleware;
 
 
 
@@ -17,7 +17,7 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200")
     .AllowCredentials());
